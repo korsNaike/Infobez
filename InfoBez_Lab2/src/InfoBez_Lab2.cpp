5a899910594bf8f910d030c87e17a898ae3cdf12
korsNaike <kirillgrinev2003@mail.ru>
2024-10-29 00:43:42 +0300

    ЛР2. Удалено лишнее

--- InfoBez_Lab2/src/InfoBez_Lab2.cpp
+++ InfoBez_Lab2/src/InfoBez_Lab2.cpp
@@ -1,44 +1,5 @@
 ﻿#include "InfoBez_Lab2.h"
-#include <iostream>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
-#include <string>
-
-#include "Access.cpp"
-#include "SparseMatrix.cpp"
-#include "SparseMatrixJsonReader.cpp"
 #include "CommandExecutor.cpp"
-
-int flex() {
-    SparseMatrixJsonReader reader("X:\\Studying\\Infobez\\InfoBez_Lab2\\data.json");
-    SparseMatrix matrix(1000, 1000);
-    reader.read(matrix);
-
-    // Вычисление процента заполненности
-    std::cout << "Fill percentage: " << matrix.getFillPercentage() << "%" << std::endl;
-
-    // Список субъектов с доступом к объекту
-    auto accessList = matrix.listSubjectsWithAccess(1);
-    std::cout << "Subjects with access to 1: ";
-    for (const auto& subject : accessList) {
-        std::cout << subject << " ";
-    }
-
-    int idPokemon = matrix.addSubject("Pokemon");
-    matrix.addSubject("Flexich");
-
-    matrix.editCell(idPokemon, 1, Access::WRITE, true);
-    std::cout << "Fill percentage: " << matrix.getFillPercentage() << "%" << std::endl;
-    for (const auto subject : matrix.subjects) {
-        std::cout << subject.first << ". " << subject.second << std::endl;
-    }
-
-    reader.write(matrix);
-    std::cout << "Wrote.." << std::endl;
-    return 1;
-}
-
 
 int main() {
     CommandExecutor executor;
